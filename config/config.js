e43ade94b721c055ba69b2ef94a916c0c6b63b63
Daniel Ogwu <ogwu.daniel.ifeanyichukwu@gmail.com>
2024-09-07 08:51:34 +0100

    update after run

--- config/config.js
+++ config/config.js
@@ -2,14 +2,25 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+// module.exports = {
+//   development: {
+//     use_env_variable: 'DATABASE_DEV_URL',
+//   },
+//   test: {
+//     use_env_variable: 'DATABASE_TEST_URL',
+//   },
+//   production: {
+//     use_env_variable: 'DATABASE_URL',
+//   },
+// };
+
+
 module.exports = {
   development: {
-    use_env_variable: 'DATABASE_DEV_URL',
-  },
-  test: {
-    use_env_variable: 'DATABASE_TEST_URL',
-  },
-  production: {
-    use_env_variable: 'DATABASE_URL',
+    username: 'postgres',
+    password: 'admin123',  // Ensure this is a string
+    database: 'izigo',
+    host: '127.0.0.1',
+    dialect: 'postgres',
   },
 };
